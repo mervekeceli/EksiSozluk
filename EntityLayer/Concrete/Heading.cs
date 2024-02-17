616fd406cec9243e4158a4b03f3244bebf9fe147
Merve Keçeli <merkec@architecht.com>
2024-02-18 01:30:25 +0300

    Tablolar arası ilişkiler kuruldu

--- EntityLayer/Concrete/Heading.cs
+++ EntityLayer/Concrete/Heading.cs
@@ -11,5 +11,13 @@ namespace EntityLayer.Concrete
         public int HeadingId { get; set; }
         public string HeadingName { get; set;}
         public DateTime HeadingDate { get;  set; }
+
+        public int CategoryId { get; set; }
+        public virtual Heading Heading { get; set; }
+
+        public int WriterId { get; set; }
+        public virtual Writer Writer { get; set; }
+
+        public ICollection<Content> Contents { get; set; }
     }
 }
