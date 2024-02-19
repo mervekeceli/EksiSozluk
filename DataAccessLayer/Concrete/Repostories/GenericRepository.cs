c40996f1995144472faa28be640055794075b41b
Merve Keçeli <merkec@architecht.com>
2024-02-19 15:17:28 +0300

    Boostrapper kullanımı yapıldı

--- DataAccessLayer/Concrete/Repostories/GenericRepository.cs
+++ DataAccessLayer/Concrete/Repostories/GenericRepository.cs
@@ -1,12 +1,50 @@
-﻿using System;
+﻿using DataAccessLayer.Abstract;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccessLayer.Concrete.Repostories
 {
-    internal class GenericRepository
+    public class GenericRepository<T> : IRepository<T> where T : class
     {
+        Context c = new Context();
+        DbSet<T> _object;
+
+        public GenericRepository()
+        {
+            //T değerinin hangi class olacağı bilgisi 
+            //Context e bağlı olarak gönderilen T değeri
+            _object=c.Set<T>();
+        }
+        public void Delete(T p)
+        {
+            _object.Add(p);
+            c.SaveChanges(); // Değişiklikleri kaydetmek için
+        }
+
+        public void Insert(T p)
+        {
+            _object.Add(p);
+            c.SaveChanges();
+        }
+
+        public List<T> List()
+        {
+            return _object.ToList();
+        }
+
+        public List<T> List(Expression<Func<T, bool>> filter)
+        {
+            return _object.Where(filter).ToList();
+        }
+
+        public void Update(T p)
+        {
+            c.SaveChanges(); // Ekrandan değerler girildi zaten sadece kaydetmesi yeterli
+        }
     }
 }
